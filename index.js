6089a4e215a861bea9d9f52d1edfe4d1e21b6cab
Jay Kumar <jaykch@outlook.com>
2016-11-13 19:31:31 +0000

    Got buttons to work using jquery

--- index.js
+++ index.js
@@ -1,31 +1,78 @@
 /**
  * Created by Jay on 11/13/2016.
  */
+$(document).ready(function(){
+    console.log("testing");
+    calculator.render(placeholder);
+    calculator.input();
+    calculator.clear();
+    calculator.add();
+    calculator.subtract();
+    calculator.multiply();
+    calculator.divide();
+});
 
-var calculator = (function(){
+var placeholder = "";
+
+
+var calculator = (function () {
+
+
+    function input(){
+        for (var i = 0; i < 10; i++) {
+            $('[data-id=' + i +']').click(function(){
+                placeholder += $(this).attr("data-id");
+                render(placeholder);
+            });
+        }
+    }
 
     function add() {
-
+        $('[data-id="add"]').click(function(){
+            placeholder = "+";
+            render(placeholder);
+            placeholder = "";
+        });
     }
 
     function subtract() {
-
+        $('[data-id="sub"]').click(function(){
+            placeholder = "-";
+            render(placeholder);
+            placeholder = "";
+        });
     }
 
     function multiply() {
-
+        $('[data-id="mult"]').click(function(){
+            placeholder = "x";
+            render(placeholder);
+            placeholder = "";
+        });
     }
 
     function divide() {
-
+        $('[data-id="div"]').click(function(){
+            placeholder = "&divide;";
+            render(placeholder);
+            placeholder = "";
+        });
     }
 
     function clear() {
-
+        $('[data-id="clear"]').click(function(){
+            placeholder = "";
+            render(placeholder);
+        });
     }
 
-    function response(){
+    function response() {
 
+    }
+    function render(x) {
+        $("#screen").html("<span>"
+            + x
+            + "</span>");
     }
 
     return {
@@ -34,7 +81,9 @@ var calculator = (function(){
         multiply: multiply,
         divide: divide,
         clear: clear,
-        response: response
+        response: response,
+        render: render,
+        input:input
     }
 
 })();
