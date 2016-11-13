be2e174d9ecedd8ba99940e9aea46a63e7ea9f8d
Jay Kumar <jaykch@outlook.com>
2016-11-13 17:01:39 +0000

    Added methods structure for calculator

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 /**
  * Created by Jay on 11/13/2016.
  */
+
+var calculator = (function(){
+
+    function add() {
+
+    }
+
+    function subtract() {
+
+    }
+
+    function multiply() {
+
+    }
+
+    function divide() {
+
+    }
+
+    function clear() {
+
+    }
+
+    function response(){
+
+    }
+
+    return {
+        add: add,
+        subtract: subtract,
+        multiply: multiply,
+        divide: divide,
+        clear: clear,
+        response: response
+    }
+
+})();
